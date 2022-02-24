db93ae255a724982adb4ea97f729eec53e7f2369
CodeBlueOcean <95150255+CodeBlueOcean@users.noreply.github.com>
2022-02-24 08:54:36 -0600

    Add files via upload

---
+++ string.indexes.py
@@ -0,0 +1,71 @@
+# example
+selfish = 'me me me'
+         # 01234567
+
+print(selfish[0])
+
+# example 
+selfish = 'me me me'
+         # 01234567
+
+print(selfish)
+
+# example 
+selfish = '01234567'
+         # 01234567
+
+#[start:stop:stepover]
+print(selfish[0:8:2])
+
+# example 
+selfish = '01234567'
+         # 01234567
+
+#[start:stop:stepover]
+print(selfish[0:8:2])
+
+# example 
+selfish = '01234567'
+         # 01234567
+
+#[start:stop:stepover]
+print(selfish[1:])
+
+
+# example 
+selfish = '01234567'
+         # 01234567
+
+#[start:stop:stepover]
+print(selfish[:5])
+
+# example 
+selfish = '01234567'
+         # 01234567
+
+#[start:stop:stepover]
+print(selfish[::1])
+
+# example 
+selfish = '01234567'
+         # 01234567
+
+#[start:stop:stepover]
+print(selfish[::-1])
+
+# example 
+selfish = '01234567'
+         # 01234567
+
+#[start:stop:stepover]
+print(selfish[-1])
+
+# example 
+selfish = '01234567'
+         # 01234567
+
+#[start:stop:stepover]
+print(selfish[::-2])
+
+
+
